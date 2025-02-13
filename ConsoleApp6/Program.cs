f20b7fd429ccaa44fa39feb9b1bffdab85da978d
yodatime <electriclikethor@gmail.com>
2025-02-13 17:17:03 +0200

    Add project files.

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ConsoleApp6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vyvedi chislo: ");
+            int number = int.Parse(Console.ReadLine());
+
+            int sum = SumOfDigits(number);
+            Console.WriteLine($"Сумата на цифрите е: {sum}");
+        }
+        static int SumOfDigits(int n)
+        {
+            if (n == 0)
+            {
+                return 0;
+            }
+            return n % 10 + SumOfDigits(n / 10);
+        }
+    }
+}
